87b044ee2422bee9d4b649351277df45d93aa910
evanmm3 <evanmm3@illinois.edu>
2020-09-24 18:16:17 -0500

    "initial commit"

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <marquee behavior = "scroll" loop = "999" >
+          Emphasis on <i><u><b> working</b></u> code </i>
+        </marquee>
         </p>
         <a
           className="App-link"
