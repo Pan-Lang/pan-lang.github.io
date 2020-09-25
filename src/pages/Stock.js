8c29707ab534daee94a1bf642f3446e919c0ca09
Renzo <renzol2@illinois.edu>
2020-09-25 16:07:29 -0500

    Add Accordion for stock insertion form

--- src/pages/Stock.js
+++ src/pages/Stock.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 import Container from 'react-bootstrap/Container';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Spinner from 'react-bootstrap/Spinner';
 import StockCard from '../components/StockCard';
 import fetchStock from '../api/Stock';
+import { Card } from 'react-bootstrap';
 
 // TODO: move all languages to a more global constant
 const LANGUAGES = ['english', 'spanish', 'french', 'mandarin'];
@@ -32,6 +34,19 @@ function Stock() {
           Language: {language}
         </Dropdown.Toggle>
 
+        <Accordion>
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey="0">
+              Click here to insert a stock item
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                Form will go here lol
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+
         <Dropdown.Menu>
           {LANGUAGES.map((lang) => (
             <Dropdown.Item
@@ -44,7 +59,7 @@ function Stock() {
           ))}
         </Dropdown.Menu>
       </Dropdown>
-      {stock.length === 0 && (
+      {stock.length === 0 && !error && (
         <Spinner animation="border" role="status">
           <span className="sr-only">Loading...</span>
         </Spinner>
