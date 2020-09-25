65d495856ec7d67d19dd2a15b9fb4957008fe172
Renzo <renzol2@illinois.edu>
2020-09-25 02:39:15 -0500

    Allow viewing stock in different languages

--- src/pages/Stock.js
+++ src/pages/Stock.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
+import Dropdown from 'react-bootstrap/Dropdown';
 import Spinner from 'react-bootstrap/Spinner';
 import StockCard from '../components/StockCard';
 import fetchStock from '../api/Stock';
+
+// TODO: move all languages to a more global constant
+const LANGUAGES = ['english', 'spanish', 'french', 'mandarin'];
 
 /**
  * Displays the stock of food pantry with options for language
@@ -10,6 +14,7 @@ import fetchStock from '../api/Stock';
 function Stock() {
   const [stock, setStock] = useState([]);
   const [error, setError] = useState(false);
+  const [language, setLanguage] = useState(LANGUAGES[0]);
 
   useEffect(() => {
     fetchStock()
@@ -22,13 +27,30 @@ function Stock() {
   return (
     <Container>
       <h1>Stock</h1>
+      <Dropdown onChange={(e) => console.log(e)}>
+        <Dropdown.Toggle id="dropdown-basic" size="lg">Language: {language}</Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          {LANGUAGES.map((lang) => (
+            <Dropdown.Item
+              onSelect={(key) => setLanguage(key)}
+              eventKey={lang}
+              key={lang}
+            >
+              {lang}
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
       {stock.length === 0 && (
         <Spinner animation="border" role="status">
           <span className="sr-only">Loading...</span>
         </Spinner>
       )}
       {stock &&
-        stock.map((item) => <StockCard stockItem={item} key={item._id} />)}
+        stock.map((item) => (
+          <StockCard stockItem={item} lang={language === 'english' ? 'name' : language} key={item._id} />
+        ))}
       {error && <p>Error :(</p>}
     </Container>
   );
