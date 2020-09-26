c3669d08761e696e2e5c1a11188cbe29a7a9f8d6
kgs5 <kgs5@illinois.edu>
2020-09-26 00:42:40 -0500

    Finished People.js and added download button

--- src/api/Client.js
+++ src/api/Client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_API_URL = 'https://panlang.herokuapp.com';
+export const BASE_API_URL = 'https://panlang.herokuapp.com';
 
 export default axios.create({
   baseURL: BASE_API_URL,
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom'
 import { View } from 'react';
-
+import { downloadInfoFile, PEOPLE_ENDPOINT } from '../api/People'
+import { BASE_API_URL } from '../api/Client';
 function Home() {
+  const [date, setDate] = useState({month: 9, year: 2020})
   return (
     <div>
-      <h1 align="center">Welcome to <font style={{color:'#26B020'}}>Pan-Lang</font>!</h1>
+      <h1 align="center">Welcome to <font style={{ color: '#26B020' }}>Pan-Lang</font>!</h1>
       <br></br>
       <div>
-      <Link to="/order" style={{color: 'white'}}>
-      <Button variant="success" size="lg" block>New Order
+        <Link to="/order" style={{ color: 'white' }}>
+          <Button variant="success" size="lg" block>New Order
       </Button>
-      </Link>
+        </Link>
       </div>
       <br></br>
       <br></br>
-      <Link to="/stock" style={{color: 'white'}}>
-      <Button variant="success" size="lg" block>Edit Stock</Button>{' '}
+      <Link to="/stock" style={{ color: 'white' }}>
+        <Button variant="success" size="lg" block>Edit Stock</Button>{' '}
       </Link>
+      {/* I think we want an anchor tag w/ href and target=_blank , 
+      if we use axios we have to hack around downloading the content for the user*/}
+      <a href={`${BASE_API_URL}${PEOPLE_ENDPOINT}?${new URLSearchParams(date)}`}>      
+      <Button variant="success" size="lg" block>Download</Button>{' '}
+      </a>
     </div>
   );
 }
