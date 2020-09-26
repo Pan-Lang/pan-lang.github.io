64bff191f0e6e1d6b1be28467fc7fcbd134a5704
Renzo <renzol2@illinois.edu>
2020-09-26 16:28:32 -0500

    Refresh page

--- src/pages/SocketTest.js
+++ src/pages/SocketTest.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import socketIOClient from 'socket.io-client';
 import Container from 'react-bootstrap/Container';
 import Loading from '../components/Loading';
+import { useHistory } from 'react-router-dom';
 import UnfulfilledOrderCard from '../components/UnfulfilledOrderCard';
 const ENDPOINT = 'http://localhost:3000'; //needs to be changed to heroku after testing
 
@@ -12,6 +13,7 @@ let listPeople = [];
  * Page with list of people with unfulfilled orders
  */
 function OrderTrackerPage() {
+  const history = useHistory();
   const [response, setResponse] = useState(listPeople);
   const socket = socketIOClient(ENDPOINT);
 
@@ -34,6 +36,8 @@ function OrderTrackerPage() {
 
   function personFulfilled(id) {
     socket.emit('personFulfilled', id);
+    // Refresh the page after emitting fulfillment
+    history.go(0);
   }
 
   return (
