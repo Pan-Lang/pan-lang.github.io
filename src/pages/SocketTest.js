07d30a0db80e15be59bc12128f839be5068ea30e
Renzo <renzol2@illinois.edu>
2020-09-26 15:18:24 -0500

    Fix socket stuff

--- src/pages/SocketTest.js
+++ src/pages/SocketTest.js
@@ -1,51 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import socketIOClient from "socket.io-client";
+import socketIOClient from 'socket.io-client';
 import Container from 'react-bootstrap/Container';
 import { Button, Col, Row } from 'react-bootstrap';
-const ENDPOINT = "http://localhost:3000"; //needs to be changed to heroku after testing
+const ENDPOINT = 'http://localhost:3000'; //needs to be changed to heroku after testing
 
-let listpeople = []
+// List of people with unfulfilled orders
+let listPeople = [];
 
 function SocketTest() {
+  const [response, setResponse] = useState(listPeople);
+  const socket = socketIOClient(ENDPOINT);
 
-  
-  
-  const [response, setResponse] = useState(listpeople);
-  const socket = socketIOClient(ENDPOINT)
-    useEffect(() => {
-      socket.on("person", data => {
-        eventHandler(data)
-        
-      })
+  useEffect(() => {
+    socket.on('person', (data) => {
+      eventHandler(data);
+    });
 
-      const eventHandler = (data) => {
-        listpeople = listpeople.concat(data)
-          setResponse(listpeople)
-      }
-      return () => {
-        console.log("effect done")
-        socket.off("person", eventHandler(""))
-      }
-        
-        
-    }, [])
+    const eventHandler = (data) => {
+      listPeople = listPeople.concat(data);
+      setResponse(listPeople);
+    };
 
-    function personFulfilled(id) {
-      socket.emit("personFulfilled", id)
-    }
-    //useState(/*this sets initial value and kind of type*/) //keeps some memory for this component
-    //useEffect()//as soon as this is rendered do all this code
-    //also needs a list so it doesn't break forever
+    return () => {
+      console.log('effect done');
+      socket.off('person', eventHandler);
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    //once they get fulfilled it needs to actually just delete it and then make a new one
+  function personFulfilled(id) {
+    socket.emit('personFulfilled', id);
+  }
+
   return (
     <Container>
       {response &&
         response.map((item, index) => (
-          <Button key={item._id} size="lg" onClick = {() => {personFulfilled(item._id)}}>
-            {item._id} {item.lastname}, {item.fulfilled.toString()},{item[index]}
+          <Button
+            key={item._id}
+            size="lg"
+            onClick={() => {
+              personFulfilled(item._id);
+            }}
+          >
+            {item._id} {item.lastname},{' '}
+            {item.fulfilled !== undefined ? item.fulfilled.toString() : null},
+            {item[index]}
           </Button>
-
         ))}
     </Container>
   );
