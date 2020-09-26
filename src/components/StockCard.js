b95b2591728c0154047180ab787dd09f9049a433
evanmm3 <evanmm3@illinois.edu>
2020-09-25 23:33:56 -0500

    graphical changes to stock

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -16,15 +16,14 @@ function StockCard({ stockItem, getStock, lang = 'name' }) {
   }, [lang, stockItem]);
 
   return (
-<<<<<<< HEAD
     <Card style={{ margin: 5 }}>
-      <Card.Body>
-        <Card.Title>
-          {hasLanguage ? stockItem[lang] : stockItem.name} 
+      <Card.Header as="h2">
+      {hasLanguage ? stockItem[lang] : stockItem.name} 
           {lang !== 'name' && hasLanguage ? " (" + stockItem['name'] + ")" : ""}
-        </Card.Title>
-        <Card.Text>Amount: {stockItem.count}</Card.Text>
-        <Card.Text style={{ textAlign: 'right' }}>
+      </Card.Header>
+      <Card.Body>
+        <Card.Text><h3>Amount: {stockItem.count}</h3></Card.Text>
+        <Card.Text style={{ textAlign: 'right'}}>
           Last updated:{' '}
           {stockItem.timestamp !== undefined
             ? new Date(stockItem.timestamp).toDateString()
@@ -35,53 +34,6 @@ function StockCard({ stockItem, getStock, lang = 'name' }) {
         )}
       </Card.Body>
     </Card>
-=======
-    <>
-      <Card style={{ margin: 5 }}>
-        <Card.Body>
-          <Card.Title>
-            {hasLanguage ? stockItem[lang] : stockItem.name}
-          </Card.Title>
-
-          <Card.Text>
-            Amount: <b>{stockItem.count}</b>
-          </Card.Text>
-          <Card.Text style={{ textAlign: 'right' }}>
-            Last updated:{' '}
-            {stockItem.timestamp !== undefined
-              ? new Date(stockItem.timestamp).toDateString()
-              : 'Unavailable'}
-          </Card.Text>
-
-          <Container
-            style={{ display: 'flex', alignItems: 'center', padding: 0 }}
-          >
-            <Button
-              size="sm"
-              variant="info"
-              style={{ alignSelf: 'center' }}
-              onClick={handleShow}
-            >
-              Edit amount
-            </Button>
-            <div style={{ margin: 'auto' }} />
-            {!hasLanguage && (
-              <Badge variant="danger">Language unavailable</Badge>
-            )}
-          </Container>
-        </Card.Body>
-      </Card>
-
-      <StockModal
-        show={showAmountModal}
-        handleClose={handleClose}
-        getStock={getStock}
-        stockName={hasLanguage ? stockItem[lang] : stockItem.name}
-        stockId={stockItem._id}
-        stockCount={stockItem.count}
-      />
-    </>
->>>>>>> 7518abb36a986386e86acf5b115010add964ec8b
   );
 }
 
