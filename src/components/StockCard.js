44c38ecbaaf72272a90a8c0a9eacf370e9498f89
Renzo <renzol2@illinois.edu>
2020-09-25 20:03:12 -0500

    Display modal when editing amount

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -1,43 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Badge, Container, Card } from 'react-bootstrap';
+import StockModal from './StockModal';
 
 function StockCard({ stockItem, lang = 'name' }) {
   const [showAmountModal, setShowAmountModal] = useState(false);
   const [hasLanguage, setHasLanguage] = useState(false);
 
+  // Handlers for showing/closing modal when editing item amount
+  const handleClose = () => setShowAmountModal(false);
+  const handleShow = () => setShowAmountModal(true);
+
+  // Determine whether this stock item has a name in the specified language
   useEffect(() => {
-    // Determine whether this stock item has a name in the specified language
     setHasLanguage(stockItem[lang] !== undefined);
   }, [lang, stockItem]);
 
   return (
-    <Card style={{ margin: 5 }}>
-      <Card.Body>
-        <Card.Title>
-          {hasLanguage ? stockItem[lang] : stockItem.name}
-        </Card.Title>
+    <>
+      <Card style={{ margin: 5 }}>
+        <Card.Body>
+          <Card.Title>
+            {hasLanguage ? stockItem[lang] : stockItem.name}
+          </Card.Title>
 
-        <Card.Text>
-          Amount: <b>{stockItem.count}</b>
-        </Card.Text>
-        <Card.Text style={{ textAlign: 'right' }}>
-          Last updated:{' '}
-          {stockItem.timestamp !== undefined
-            ? new Date(stockItem.timestamp).toDateString()
-            : 'Unavailable'}
-        </Card.Text>
+          <Card.Text>
+            Amount: <b>{stockItem.count}</b>
+          </Card.Text>
+          <Card.Text style={{ textAlign: 'right' }}>
+            Last updated:{' '}
+            {stockItem.timestamp !== undefined
+              ? new Date(stockItem.timestamp).toDateString()
+              : 'Unavailable'}
+          </Card.Text>
 
-        <Container
-          style={{ display: 'flex', alignItems: 'center', padding: 0 }}
-        >
-          <Button size="sm" variant="info" style={{ alignSelf: 'center' }}>
-            Edit amount
-          </Button>
-          <div style={{ margin: 'auto' }} />
-          {!hasLanguage && <Badge variant="danger">Language unavailable</Badge>}
-        </Container>
-      </Card.Body>
-    </Card>
+          <Container
+            style={{ display: 'flex', alignItems: 'center', padding: 0 }}
+          >
+            <Button
+              size="sm"
+              variant="info"
+              style={{ alignSelf: 'center' }}
+              onClick={handleShow}
+            >
+              Edit amount
+            </Button>
+            <div style={{ margin: 'auto' }} />
+            {!hasLanguage && (
+              <Badge variant="danger">Language unavailable</Badge>
+            )}
+          </Container>
+        </Card.Body>
+      </Card>
+
+      <StockModal show={showAmountModal} handleClose={handleClose} />
+    </>
   );
 }
 
---
+++ src/components/StockModal.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+function StockModal({ show, handleClose }) {
+  return (
+    <Modal
+      show={show}
+      onHide={handleClose}
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Edit amount
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Test</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleClose}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default StockModal;
