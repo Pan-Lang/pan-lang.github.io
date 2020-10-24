bcb3c8768f751415bb75c338fb690d87612a32c1
Kevin Zhou <qwertycowmoo@gmail.com>
2020-10-24 17:10:11 -0500

    ordersList is not working as intended, only showing the first result instead of all

--- src/pages/OrderTracker.js
+++ src/pages/OrderTracker.js
@@ -13,13 +13,25 @@ function OrderTracker() {
   useEffect(() => {
     const socket = socketIOClient(BASE_API_URL);
     socket.on('person', (data) => {
-      console.log(data);
-      eventHandler(data);
+      eventHandler(data); //has this been properly hooked?
     });
 
     const eventHandler = (personData) => {
-      setOrdersList(ordersList.concat(personData));
+      console.log(personData);
+      console.log("inside eventhandler")
       console.log(ordersList);
+      // var copyOrders = ordersList
+      // copyOrders.push(personData)
+      // setOrdersList(copyOrders);
+
+      //currently this only displays the most recent order instead of all fulfilled orders
+      //The commented code above actually adds it to the setOrdersList but the return function doesn't like recognize it?
+      setOrdersList(ordersList.concat(personData));
+      //ordersList doesn't like have anything in it?
+      ordersList.forEach((order) => {
+        console.log("hubba")
+        console.log(order)
+      })
     };
 
     return () => {
