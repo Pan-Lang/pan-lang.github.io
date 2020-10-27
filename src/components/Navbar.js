3c4728b0a0d65e9fa8cc9fa52e1461af27702c08
Renzo <renzol2@illinois.edu>
2020-10-27 16:26:42 -0500

    Collapse responsive navbar upon selection

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,19 +5,31 @@ import Nav from 'react-bootstrap/Nav';
 
 function NavigationBar() {
   return (
-    <Navbar bg="light" expand="lg" fixed="top">
+    <Navbar collapseOnSelect={true} bg="light" expand="lg" fixed="top">
       <Navbar.Brand href="#home" as={Link} to="/">
         <div id="logo">
-          <img src={require('../images/logo.png')} alt="Pan-Lang" style={{width:150, height:45}}></img>
+          <img
+            src={require('../images/logo.png')}
+            alt="Pan-Lang"
+            style={{ width: 150, height: 45 }}
+          ></img>
         </div>
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/order">Order</Nav.Link>
-          <Nav.Link as={Link} to="/stock">Stock</Nav.Link>
-          <Nav.Link as={Link} to="/order-tracker">Order Tracker</Nav.Link>
-          <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          <Nav.Link eventKey="0" as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link eventKey="1" as={Link} to="/order">
+            Order
+          </Nav.Link>
+          <Nav.Link eventKey="2" as={Link} to="/stock">
+            Stock
+          </Nav.Link>
+          <Nav.Link eventKey="3" as={Link} to="/order-tracker">
+            Order Tracker
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
