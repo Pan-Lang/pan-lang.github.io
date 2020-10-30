10faddd6116cc3da8973ba4c5d94000b9f7c7d89
Renzo <renzol2@illinois.edu>
2020-10-30 12:50:27 -0500

    Center the spinner

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import Spinner from 'react-bootstrap/Spinner';
 
 function Loading() {
   return (
-    <Spinner animation="border" role="status">
-      <span className="sr-only">Loading...</span>
-    </Spinner>
+    <Container style={{ display: 'flex', justifyContent: 'center' }}>
+      <Spinner
+        animation="border"
+        role="status"
+        className="mt-3"
+        style={{ marginLeft: 'auto', marginRight: 'auto' }}
+      >
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    </Container>
   );
 }
 
