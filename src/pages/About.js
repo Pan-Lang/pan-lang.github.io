4caf413245f75270b0c0e8178421bf9dc0f1ade5
Renzo <renzol2@illinois.edu>
2020-10-30 11:50:13 -0500

    Reformat About page to fit with new UI changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,34 +3,31 @@ import Container from 'react-bootstrap/Container';
 
 function About() {
   return (
-    <Container style={{ backgroundColor: 'white', paddingBottom: 120 }}>
+    <Container style={{ paddingBottom: 120, backgroundColor: 'white' }}>
       <div style={{ marginBottom: 20 }}>
-        <h1>About </h1>{' '}
         <h1>
+          {'About '}
           <font style={{ color: '#26B020', fontWeight: 'bold' }}>
             Pan-Lang:
           </font>
         </h1>
-        <body>
-          <p>
-            Pan-Lang is an organizational web application designed to help local
-            food bank organizers keep track of orders, maintain visitor records, and
-            communicate with non-English speaking patrons.
-          </p>
-          <p>
-            Food insecurity is an important issue in the Champain County
-            community. Nearly 15% of residents must deal with food insecurity.
-            Many of these individuals are also ineligible for federal nutrition
-            programs and must rely on local food pantries for meals.
-          </p>
-          <p>
-            On top of this, many of these individuals speak limited English,
-            making it difficult for them to communicate their needs to food
-            pantry staff. With Pan-Lang, our mission is to break this language
-            barrier and provide a more efficient method of supporting those in
-            need.
-          </p>
-        </body>
+        <p>
+          Pan-Lang is an organizational web application designed to help local
+          food bank organizers keep track of orders, maintain visitor records,
+          and communicate with non-English speaking patrons.
+        </p>
+        <p>
+          Food insecurity is an important issue in the Champain County
+          community. Nearly 15% of residents must deal with food insecurity.
+          Many of these individuals are also ineligible for federal nutrition
+          programs and must rely on local food pantries for meals.
+        </p>
+        <p>
+          On top of this, many of these individuals speak limited English,
+          making it difficult for them to communicate their needs to food pantry
+          staff. With Pan-Lang, our mission is to break this language barrier
+          and provide a more efficient method of supporting those in need.
+        </p>
       </div>
     </Container>
   );
