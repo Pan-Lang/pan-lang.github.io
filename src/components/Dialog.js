83cf7325efc26408c0198d3090ca3865cd54ba6b
Nithinps021 <47279496+Nithinps021@users.noreply.github.com>
2020-12-15 15:14:36 +0530

    added image and made responsive

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -6,36 +6,54 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import cart from '../images/empty-cart.svg'
+import Grid from '@material-ui/core/Grid';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+
 const useStyles = makeStyles((theme) => ({
-    button:{
+    button: {
         justifyContent: 'center',
-        marginBottom:theme.spacing(1)
+        marginBottom: theme.spacing(1)
     },
-    dialog:{
-        margin:theme.spacing(10),
+    dialog: {
+        margin: theme.spacing(10),
+        display: 'flex',
+        flexDirection: 'column',
+        margin: 'auto',
+        width: 'fit-content',
+    },
+    image: {
+        width: theme.spacing(25),
+        height: theme.spacing(25),
+        marginTop: theme.spacing(20)
+    },
+    grid: {
+        textAlign: 'center',
     }
 }));
 
 const Theme = createMuiTheme({
     palette: {
-      primary: {
-        main: "#00c853",
-      },
-      secondary: {
-        main: "#00c853",
-      },
-      action:{
-          main:"#00c853"
-      }
+        primary: {
+            main: "#00c853",
+        },
+        secondary: {
+            main: "#00c853",
+        },
+        action: {
+            main: "#00c853"
+        }
     },
-  });
+});
 
 
 
 export default function AlertDialogSlide() {
     const classes = useStyles();
     const [open, setOpen] = React.useState(true);
-
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
     const handleClose = () => {
         setOpen(false);
     };
@@ -43,22 +61,34 @@ export default function AlertDialogSlide() {
     return (
         <div>
             <MuiThemeProvider theme={Theme}>
-            <Dialog
-                open={open}
-                keepMounted
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-slide-title"
-                aria-describedby="alert-dialog-slide-description"
-                className={classes.dialog}
-            >
-                <DialogTitle id="alert-dialog-slide-title">{"No orders at the moment"}</DialogTitle>
-                <DialogActions className={classes.button}>
-                    <Button onClick={handleClose} color="primary" variant='contained'>
-                        OK
+                <Dialog
+                    open={open}
+                    maxWidth='lg'
+                    keepMounted
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-slide-title"
+                    aria-describedby="alert-dialog-slide-description"
+                    className={classes.dialog}
+                >
+                    <DialogTitle id="alert-dialog-slide-title">{"No orders at the moment"}</DialogTitle>
+                    <DialogActions className={classes.button}>
+                        <Button onClick={handleClose} color="primary" variant='contained'>
+                            OK
                     </Button>
-                </DialogActions>
-            </Dialog>
+                    </DialogActions>
+                </Dialog>
             </MuiThemeProvider>
+            <Grid container className={classes.grid}>
+                <Grid item lg={4} sm={4}></Grid>
+                <Grid item xs={12} lg={4} sm={4} className={classes.grid}>
+                    <img src={cart} className={classes.image}></img>
+                </Grid>
+                <Grid item lg={4} sm={4}></Grid>
+
+            </Grid>
+
+
+
         </div>
     );
 }
