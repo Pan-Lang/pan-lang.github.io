8cf2ec5ba67bbfa16d7110c884c495a4f81fe826
Renzo <renzol2@illinois.edu>
2020-12-21 22:52:24 -0600

    Hide navbar on scroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { AppBar, Toolbar } from '@material-ui/core';
 import CollapsingMenu from './CollapsingMenu';
 import { makeStyles } from '@material-ui/core/styles';
+import Slide from '@material-ui/core/Slide';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,23 +29,26 @@ const useStyles = makeStyles((theme) => ({
  */
 function Navbar() {
   const classes = useStyles();
+  const trigger = useScrollTrigger();
 
   return (
-    <AppBar position="fixed" className={classes.navigation}>
-      <Toolbar>
-        {/* Logo */}
-        <Link to="/">
-          <img
-            src={require('../../images/logo_nobg.png')}
-            alt="Pan-Lang"
-            style={{ width: 75, height: 75 }}
-          />
-        </Link>
+    <Slide appear={false} direction="down" in={!trigger}>
+      <AppBar className={classes.navigation}>
+        <Toolbar>
+          {/* Logo */}
+          <Link to="/">
+            <img
+              src={require('../../images/logo_nobg.png')}
+              alt="Pan-Lang"
+              style={{ width: 75, height: 75 }}
+            />
+          </Link>
 
-        {/* Navigation menu */}
-        <CollapsingMenu />
-      </Toolbar>
-    </AppBar>
+          {/* Navigation menu */}
+          <CollapsingMenu />
+        </Toolbar>
+      </AppBar>
+    </Slide>
   );
 }
 
