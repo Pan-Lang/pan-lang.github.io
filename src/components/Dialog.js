7fe00dbfc6fe55cbe79230e1ecebd57d942ae081
Renzo <renzol2@illinois.edu>
2020-12-21 19:58:45 -0600

    Import AppBar as navigation from material ui

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -4,91 +4,87 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-import { createMuiTheme } from "@material-ui/core/styles";
-import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
-import cart from '../images/empty-cart.svg'
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import cart from '../images/empty-cart.svg';
 import Grid from '@material-ui/core/Grid';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    button: {
-        justifyContent: 'center',
-        marginBottom: theme.spacing(1)
-    },
-    dialog: {
-        margin: theme.spacing(10),
-        display: 'flex',
-        flexDirection: 'column',
-        margin: 'auto',
-        width: 'fit-content',
-    },
-    image: {
-        width: theme.spacing(25),
-        height: theme.spacing(25),
-        marginTop: theme.spacing(20)
-    },
-    grid: {
-        textAlign: 'center',
-    }
+  button: {
+    justifyContent: 'center',
+    marginBottom: theme.spacing(1),
+  },
+  dialog: {
+    margin: theme.spacing(10),
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 'auto',
+    width: 'fit-content',
+  },
+  image: {
+    width: theme.spacing(25),
+    height: theme.spacing(25),
+    marginTop: theme.spacing(20),
+  },
+  grid: {
+    textAlign: 'center',
+  },
 }));
 
 const Theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: "#00c853",
-        },
-        secondary: {
-            main: "#00c853",
-        },
-        action: {
-            main: "#00c853"
-        }
+  palette: {
+    primary: {
+      main: '#00c853',
     },
+    secondary: {
+      main: '#00c853',
+    },
+    action: {
+      main: '#00c853',
+    },
+  },
 });
 
-
-
 export default function AlertDialogSlide() {
-    const classes = useStyles();
-    const [open, setOpen] = React.useState(true);
-    const theme = useTheme();
-    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    return (
-        <div>
-            <MuiThemeProvider theme={Theme}>
-                <Dialog
-                    open={open}
-                    maxWidth='lg'
-                    keepMounted
-                    onClose={handleClose}
-                    aria-labelledby="alert-dialog-slide-title"
-                    aria-describedby="alert-dialog-slide-description"
-                    className={classes.dialog}
-                >
-                    <DialogTitle id="alert-dialog-slide-title">{"No orders at the moment"}</DialogTitle>
-                    <DialogActions className={classes.button}>
-                        <Button onClick={handleClose} color="primary" variant='contained'>
-                            OK
-                    </Button>
-                    </DialogActions>
-                </Dialog>
-            </MuiThemeProvider>
-            <Grid container className={classes.grid}>
-                <Grid item lg={4} sm={4}></Grid>
-                <Grid item xs={12} lg={4} sm={4} className={classes.grid}>
-                    <img src={cart} className={classes.image}></img>
-                </Grid>
-                <Grid item lg={4} sm={4}></Grid>
-
-            </Grid>
-
-
-
-        </div>
-    );
+  return (
+    <div>
+      <MuiThemeProvider theme={Theme}>
+        <Dialog
+          open={open}
+          maxWidth="lg"
+          keepMounted
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+          className={classes.dialog}
+        >
+          <DialogTitle id="alert-dialog-slide-title">
+            {'No orders at the moment'}
+          </DialogTitle>
+          <DialogActions className={classes.button}>
+            <Button onClick={handleClose} color="primary" variant="contained">
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </MuiThemeProvider>
+      <Grid container className={classes.grid}>
+        <Grid item lg={4} sm={4}></Grid>
+        <Grid item xs={12} lg={4} sm={4} className={classes.grid}>
+          <img src={cart} className={classes.image}></img>
+        </Grid>
+        <Grid item lg={4} sm={4}></Grid>
+      </Grid>
+    </div>
+  );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,92 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import {
+  AppBar,
+  Toolbar,
+  Link as MaterialLink,
+  Button,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-function NavigationBar() {
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+    backgroundColor: 'white',
+  },
+}));
+
+function NavBar() {
+  const classes = useStyles();
+
   return (
-    <Navbar collapseOnSelect={true} bg="light" expand="lg" fixed="top">
-      <Navbar.Brand href="#home" as={Link} to="/">
-        <div id="logo">
+    <AppBar className={classes.appBar} position="static">
+      <Toolbar className={classes.toolbar}>
+        <Link to="/">
           <img
             src={require('../images/logo_nobg.png')}
             alt="Pan-Lang"
             style={{ width: 75, height: 75 }}
-          ></img>
-        </div>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link eventKey="0" as={Link} to="/">
-            Home
-          </Nav.Link>
-          <Nav.Link eventKey="1" as={Link} to="/about">
+          />
+        </Link>
+        <nav>
+          <MaterialLink component={Link} variant="button" to="/about">
             About
-          </Nav.Link>
-          <Nav.Link eventKey="2" as={Link} to="/order">
-            Order
-          </Nav.Link>
-          <Nav.Link eventKey="3" as={Link} to="/stock">
-            Stock
-          </Nav.Link>
-          <Nav.Link eventKey="4" as={Link} to="/order-tracker">
+          </MaterialLink>
+          <MaterialLink component={Link} variant="button" to="/order">
+            Order Form
+          </MaterialLink>
+          <MaterialLink component={Link} variant="button" to="/order-tracker">
             Order Tracker
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+          </MaterialLink>
+          <MaterialLink component={Link} variant="button" to="/stock">
+            Stock Dashboard
+          </MaterialLink>
+        </nav>
+        <Button href="#" variant="outlined">
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 }
 
-export default NavigationBar;
+export default NavBar;
+
+// function NavigationBar() {
+//   return (
+//     <Navbar collapseOnSelect={true} bg="light" expand="lg" fixed="top">
+//       <Navbar.Brand href="#home" as={Link} to="/">
+//         <div>
+//           <img
+//             src={require('../images/logo_nobg.png')}
+//             alt="Pan-Lang"
+//             style={{ width: 75, height: 75 }}
+//           ></img>
+//         </div>
+//       </Navbar.Brand>
+//       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+//       <Navbar.Collapse id="responsive-navbar-nav">
+//         <Nav className="mr-auto">
+//           <Nav.Link eventKey="0" as={Link} to="/">
+//             Home
+//           </Nav.Link>
+//           <Nav.Link eventKey="1" as={Link} to="/about">
+//             About
+//           </Nav.Link>
+//           <Nav.Link eventKey="2" as={Link} to="/order">
+//             Order
+//           </Nav.Link>
+//           <Nav.Link eventKey="3" as={Link} to="/stock">
+//             Stock
+//           </Nav.Link>
+//           <Nav.Link eventKey="4" as={Link} to="/order-tracker">
+//             Order Tracker
+//           </Nav.Link>
+//         </Nav>
+//       </Navbar.Collapse>
+//     </Navbar>
+//   );
+// }
