9bfb8929a84f68492a0ffe9d97723533e2306a6f
Renzo <renzol2@illinois.edu>
2020-12-23 16:31:51 -0600

    Create card component to show team

---
+++ src/components/AvatarCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Avatar, Container, makeStyles, Paper, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    [theme.breakpoints.down('sm')]: {
+      padding: 5,
+      width: '100%',
+    },
+  },
+  avatar: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: theme.spacing(25),
+    height: theme.spacing(25),
+    [theme.breakpoints.down('sm')]: {
+      width: theme.spacing(10),
+      height: theme.spacing(10),
+    },
+    marginBottom: 10,
+  },
+}));
+
+function AvatarCard({ name, src, alt }) {
+  const classes = useStyles();
+
+  return (
+    <Paper elevation={3} className={classes.card}>
+      <Container>
+        <Avatar src={src} alt={alt} className={classes.avatar} />
+        <Typography variant="h5">{name}</Typography>
+        <Typography variant="subtitle1">Developer</Typography>
+      </Container>
+    </Paper>
+  );
+}
+
+export default AvatarCard;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,45 +1,92 @@
 import React from 'react';
 import { Container, makeStyles, Typography } from '@material-ui/core';
+import AvatarCard from '../components/AvatarCard';
 
 const useStyles = makeStyles((theme) => ({
+  page: {
+    backgroundColor: 'white',
+  },
   p: {
     marginTop: 8,
     marginBottom: 8,
   },
+  team: {
+    textAlign: 'center',
+    width: '100%',
+    paddingBottom: 50,
+  },
+  avatarGroup: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'stretch',
+    justifyContent: 'space-around',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
 }));
 
+/**
+ * About page for Pan-Lang
+ * Provides background for our application and service
+ */
 function About() {
   const classes = useStyles();
+  const team = [
+    { name: 'Kevin Zhou' },
+    { name: 'Katie Sanders' },
+    { name: 'Renzo Ledesma' },
+    { name: 'Evan Matthews' },
+  ];
+
   return (
-    <Container
-      maxWidth="md"
-      style={{
-        paddingBottom: 120,
-        marginBottom: 20,
-        backgroundColor: 'white',
-      }}
-    >
-      <Typography variant="h2">
-        {'About '}
-        <font style={{ color: '#26B020', fontWeight: 'bold' }}>Pan-Lang:</font>
-      </Typography>
-      <Typography component="p" className={classes.p}>
-        Pan-Lang is an organizational web application designed to help local
-        food bank organizers keep track of orders, maintain visitor records, and
-        communicate with non-English speaking patrons.
-      </Typography>
-      <Typography component="p" className={classes.p}>
-        Food insecurity is an important issue in the Champain County community.
-        Nearly 15% of residents must deal with food insecurity. Many of these
-        individuals are also ineligible for federal nutrition programs and must
-        rely on local food pantries for meals.
-      </Typography>
-      <Typography component="p" className={classes.p}>
-        On top of this, many of these individuals speak limited English, making
-        it difficult for them to communicate their needs to food pantry staff.
-        With Pan-Lang, our mission is to break this language barrier and provide
-        a more efficient method of supporting those in need.
-      </Typography>
+    <Container className={classes.page}>
+      <Container
+        maxWidth="md"
+        style={{
+          paddingBottom: 120,
+          marginBottom: 20,
+        }}
+      >
+        {/* Background */}
+        <Typography variant="h2">
+          {'About '}
+          <font style={{ color: '#26B020', fontWeight: 'bold' }}>
+            Pan-Lang:
+          </font>
+        </Typography>
+        <Typography component="p" className={classes.p}>
+          Pan-Lang is an organizational web application designed to help local
+          food bank organizers keep track of orders, maintain visitor records,
+          and communicate with non-English speaking patrons.
+        </Typography>
+        <Typography component="p" className={classes.p}>
+          Food insecurity is an important issue in the Champain County
+          community. Nearly 15% of residents must deal with food insecurity.
+          Many of these individuals are also ineligible for federal nutrition
+          programs and must rely on local food pantries for meals.
+        </Typography>
+        <Typography component="p" className={classes.p}>
+          On top of this, many of these individuals speak limited English,
+          making it difficult for them to communicate their needs to food pantry
+          staff. With Pan-Lang, our mission is to break this language barrier
+          and provide a more efficient method of supporting those in need.
+        </Typography>
+      </Container>
+
+      {/* Team */}
+      <Container className={classes.team}>
+        <Typography variant="h3">Our Team</Typography>
+        <Container className={classes.avatarGroup}>
+          {team.map((member) => (
+            <AvatarCard
+              name={member.name}
+              src={'/src/images/logo.png'}
+              alt={member.name}
+            />
+          ))}
+        </Container>
+      </Container>
     </Container>
   );
 }
