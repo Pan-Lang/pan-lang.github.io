3b326dcb04e936d0f10fd17f0a545219a3d12104
Renzo <renzol2@illinois.edu>
2020-12-22 21:48:07 -0600

    Use MUI components in About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,34 +1,45 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
+import { Container, makeStyles, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  p: {
+    marginTop: 8,
+    marginBottom: 8,
+  },
+}));
 
 function About() {
+  const classes = useStyles();
   return (
-    <Container style={{ paddingBottom: 120, backgroundColor: 'white' }}>
-      <div style={{ marginBottom: 20 }}>
-        <h1>
-          {'About '}
-          <font style={{ color: '#26B020', fontWeight: 'bold' }}>
-            Pan-Lang:
-          </font>
-        </h1>
-        <p>
-          Pan-Lang is an organizational web application designed to help local
-          food bank organizers keep track of orders, maintain visitor records,
-          and communicate with non-English speaking patrons.
-        </p>
-        <p>
-          Food insecurity is an important issue in the Champain County
-          community. Nearly 15% of residents must deal with food insecurity.
-          Many of these individuals are also ineligible for federal nutrition
-          programs and must rely on local food pantries for meals.
-        </p>
-        <p>
-          On top of this, many of these individuals speak limited English,
-          making it difficult for them to communicate their needs to food pantry
-          staff. With Pan-Lang, our mission is to break this language barrier
-          and provide a more efficient method of supporting those in need.
-        </p>
-      </div>
+    <Container
+      maxWidth="md"
+      style={{
+        paddingBottom: 120,
+        marginBottom: 20,
+        backgroundColor: 'white',
+      }}
+    >
+      <Typography variant="h2">
+        {'About '}
+        <font style={{ color: '#26B020', fontWeight: 'bold' }}>Pan-Lang:</font>
+      </Typography>
+      <Typography component="p" className={classes.p}>
+        Pan-Lang is an organizational web application designed to help local
+        food bank organizers keep track of orders, maintain visitor records, and
+        communicate with non-English speaking patrons.
+      </Typography>
+      <Typography component="p" className={classes.p}>
+        Food insecurity is an important issue in the Champain County community.
+        Nearly 15% of residents must deal with food insecurity. Many of these
+        individuals are also ineligible for federal nutrition programs and must
+        rely on local food pantries for meals.
+      </Typography>
+      <Typography component="p" className={classes.p}>
+        On top of this, many of these individuals speak limited English, making
+        it difficult for them to communicate their needs to food pantry staff.
+        With Pan-Lang, our mission is to break this language barrier and provide
+        a more efficient method of supporting those in need.
+      </Typography>
     </Container>
   );
 }
