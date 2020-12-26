650e8bc75d3daa6ee12ba76a8ba26d2bf2347b13
Renzo <renzol2@illinois.edu>
2020-12-26 17:34:01 -0600

    Refactor stock input form component with MUI

--- src/components/StockInput.js
+++ src/components/StockInput.js
@@ -1,82 +1,113 @@
 import React from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { Formik } from 'formik';
+import { withFormik } from 'formik';
 import * as Yup from 'yup';
-import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
-import { createStockItem } from '../api/Stock';
-
-const stockSchema = Yup.object({
-  name: Yup.string().required(),
-  count: Yup.number().integer().moreThan(-1).required(),
-});
+import { makeStyles } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 /**
  * Handles creation of new stock items
  */
-function StockInput({ getStock }) {
+function StockInputForm({
+  values,
+  touched,
+  errors,
+  isSubmitting,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  handleReset,
+}) {
+  const classes = useStyles();
   return (
-    <Accordion>
-      <Card>
-        <Accordion.Toggle as={Card.Header} eventKey="0">
-          Click here to insert a stock item
-        </Accordion.Toggle>
-        <Accordion.Collapse eventKey="0">
-          <Card.Body>
-            <Formik
-              validationSchema={stockSchema}
-              onSubmit={(item) => {
-                getStock(500);
-                console.log(createStockItem(item));
-              }}
-              initialValues={{
-                name: '',
-                count: -1,
-              }}
-            >
-              {({
-                handleSubmit,
-                handleChange,
-                handleBlur,
-                values,
-                touched,
-                isValid,
-                errors,
-              }) => (
-                <Form noValidate onSubmit={handleSubmit}>
-                  <Form.Group controlId="stockInputName">
-                    <Form.Control
-                      type="text"
-                      name="name"
-                      placeholder={'Insert item name here'}
-                      onChange={handleChange}
-                      isValid={touched.name && !errors.name}
-                      isInvalid={!!errors.name}
-                    />
-                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                  </Form.Group>
-                  <Form.Group controlId="stockInputCount">
-                    <Form.Control
-                      type="number"
-                      name="count"
-                      placeholder={'Insert item count here'}
-                      onChange={handleChange}
-                      isValid={touched.count && !errors.count}
-                      isInvalid={!!errors.count}
-                    />
-                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                  </Form.Group>
-                  <Button variant="success" type="submit">
-                    Add item to database
-                  </Button>
-                </Form>
-              )}
-            </Formik>
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    </Accordion>
+    <Container className={classes.container}>
+      <form onSubmit={handleSubmit}>
+        <Card className={classes.card}>
+          <CardContent>
+            <TextField
+              id="itemName"
+              label="New item name"
+              value={values.itemName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              helperText={touched.itemName ? errors.itemName : ''}
+              error={touched.itemName && Boolean(errors.itemName)}
+              margin="dense"
+              variant="outlined"
+              fullWidth
+            />
+            <TextField
+              id="itemCount"
+              label="New item count"
+              value={values.itemCount}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              helperText={touched.itemCount ? errors.itemCount : ''}
+              error={touched.itemCount && Boolean(errors.itemCount)}
+              margin="dense"
+              variant="outlined"
+              fullWidth
+            />
+          </CardContent>
+          <CardActions className={classes.actions}>
+            <Button type="submit" color="primary" disabled={isSubmitting}>
+              SUBMIT
+            </Button>
+            <Button color="secondary" onClick={handleReset}>
+              CLEAR
+            </Button>
+          </CardActions>
+        </Card>
+      </form>
+    </Container>
   );
 }
+
+// Formik wrapper for component
+const StockInput = withFormik({
+  mapPropsToValues: ({
+    itemName,
+    itemCount,
+  }) => {
+    return {
+      itemName: itemName || '',
+      itemCount: itemCount || 1,
+    };
+  },
+
+  validationSchema: Yup.object({
+    itemName: Yup.string().required(),
+    itemCount: Yup.number().integer().moreThan(-1).required(),
+  }),
+
+  handleSubmit: (values, { setSubmitting }) => {
+    setTimeout(() => {
+      // submit to the server
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 1000);
+  }
+})(StockInputForm);
+
+// Styling
+const useStyles = makeStyles((theme) => ({
+  container: {
+    width: '100%',
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
+  card: {
+    width: '100%',
+    marginTop: 5,
+  },
+  actions: {
+    float: 'right',
+  },
+}));
+
 
 export default StockInput;
