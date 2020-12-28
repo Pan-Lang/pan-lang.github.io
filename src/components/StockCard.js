39618092f2b0237d42a980b59a69152365713000
Renzo <renzol2@illinois.edu>
2020-12-27 23:05:21 -0600

    Build new stock card design

--- src/components/StockCard.js
+++ src/components/StockCard.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import Chip from '@material-ui/core/Chip';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
@@ -37,52 +38,63 @@ function StockCard({ stockItem, getStock, lang = 'name' }) {
   return (
     <>
       <Fade in={true}>
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography variant="h4" className={classes.name}>
-              {hasLanguage ? stockItem[lang] : stockItem.name}
-              {lang !== 'name' && hasLanguage
-                ? ' (' + stockItem['name'] + ')'
-                : ''}
-            </Typography>
+        <Card className={classes.card} onClick={handleShow}>
+          {/* Action area makes entire component focusable */}
+          <CardActionArea>
+            <CardContent>
+              {/* Top content of card */}
+              <Container className={classes.topContent}>
+                {/* Name and translation (left) */}
+                <Box className={classes.nameContainer}>
+                  {/* Name */}
+                  <Typography variant="h4" className={classes.name}>
+                    {hasLanguage ? stockItem[lang] : stockItem.name}
+                  </Typography>
 
-            <Typography>
-              Amount:{' '}
-              <font style={{ fontWeight: 'bolder' }}>{stockItem.count}</font>
-            </Typography>
+                  {/* Translation */}
+                  <Typography variant="h5" className={classes.subname}>
+                    {hasLanguage && lang !== 'name' ? stockItem.name : ''}
+                  </Typography>
 
-            <Typography style={{ textAlign: 'right' }}>
-              Last updated:{' '}
-              {stockItem.timestamp !== undefined
-                ? getItemDateString(stockItem)
-                : 'Unavailable'}
-            </Typography>
+                  {/* No translation alert */}
+                  {!hasLanguage && (
+                    <Chip
+                      className={classes.noTranslation}
+                      size="small"
+                      color="secondary"
+                      label={'Translation unavailable'}
+                    />
+                  )}
+                </Box>
 
-            {stockItem.count <= 0 && (
-              <ErrorAlert body="Warning: Out of stock" />
-            )}
-          </CardContent>
-          <CardActions>
-            <Container
-              style={{ display: 'flex', alignItems: 'center', padding: 0 }}
-            >
-              {!hasLanguage && (
-                <Chip
-                  size="small"
-                  color="secondary"
-                  label={'Language unavailable'}
-                />
-              )}
-              <div style={{ margin: 'auto' }} />
-              <Button
-                size="small"
-                className={classes.button}
-                onClick={handleShow}
-              >
-                Edit amount
-              </Button>
-            </Container>
-          </CardActions>
+                {/* Count (right) */}
+                <Box className={classes.countContainer}>
+                  <Typography className={classes.countHeader}>Count</Typography>
+                  <Typography className={classes.count}>
+                    {stockItem.count}
+                  </Typography>
+                </Box>
+              </Container>
+              <Container className={classes.bottomContent}>
+                {/* Date */}
+                <Typography className={classes.date}>
+                  Last updated:{' '}
+                  {stockItem.timestamp !== undefined
+                    ? getItemDateString(stockItem)
+                    : 'Unavailable'}
+                </Typography>
+
+                {/* Out of stock alert */}
+                {stockItem.count <= 0 && (
+                  <Chip
+                    size="small"
+                    color="secondary"
+                    label={'Out of stock'}
+                  />
+                )}
+              </Container>
+            </CardContent>
+          </CardActionArea>
         </Card>
       </Fade>
 
@@ -101,17 +113,59 @@ function StockCard({ stockItem, getStock, lang = 'name' }) {
 const useStyles = makeStyles((theme) => ({
   card: {
     margin: theme.spacing(1),
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-    paddingTop: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
-    borderRadius: '50px',
+    borderRadius: '30px',
   },
+  cardAction: {
+    paddingLeft: theme.spacing(0),
+    paddingRight: theme.spacing(0),
+    paddingTop: theme.spacing(0),
+    paddingBottom: theme.spacing(0),
+  },
+  topContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  bottomContent: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+  },
+  nameContainer: {},
   name: {
-    fontWeight: 'bold', 
+    fontWeight: 'bold',
+    fontSize: '30px',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '22px',
+    }
+  },
+  noTranslation: {
+    marginTop: 5,
   },
   subname: {
-    fontSize: theme.typography.caption.fontSize,
+    marginTop: -8,
+    [theme.breakpoints.down('md')]: {
+      fontSize: '18px',
+      marginTop: -4,
+    }
+  },
+  date: {
+    color: 'gray',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '12px',
+    }
+  },
+  countContainer: {},
+  countHeader: {
+    textAlign: 'right',
+  },
+  count: {
+    marginTop: -10,
+    fontWeight: 'bold',
+    fontSize: '40px',
+    textAlign: 'right',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '27px',
+    }
   },
   button: {
     alignSelf: 'center',
