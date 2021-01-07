322b8968c746e835ac2c32aa2e8c1421b0cc6087
Renzo <renzol2@illinois.edu>
2021-01-07 16:38:17 -0600

    Use env variables over config file

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,18 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import firebaseConfig from './firebaseConfig.json';
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const env = process.env;
+firebase.initializeApp({
+  apiKey: env.REACT_APP_API_KEY,
+  authDomain: env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: env.REACT_APP_DATABASE_URL,
+  projectId: env.REACT_APP_PROJECT_ID,
+  storageBucket: env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: env.REACT_APP_APP_ID,
+  measurementId: env.REACT_APP_MEASUREMENT_ID,
+});
 
 export const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,21 +13,40 @@ function Home() {
   const [user] = useAuthState(auth);
 
   if (!Boolean(user)) {
-    return <Container>We'll make this pretty later, just sign in.</Container>;
-  }
-
-  else return (
-    <Container style={{ textAlign: 'center' }}>
-      <div style={{ marginBottom: 20 }}>
-        <br></br>
-        <h1>Welcome to </h1>{' '}
-        <h1>
-          <font style={{ color: '#35E82A', fontWeight: 'bold' }}>Pan-</font>
-          <font style={{ color: '#2EFFD5', fontWeight: 'bold' }}>Lang</font>
-        </h1>
-      </div>
-      <div>
-        <Link to={ORDER_FORM} style={{ color: 'white' }}>
+    return (
+      <Container>
+        <p>We'll make this pretty later, just sign in.</p>
+      </Container>
+    );
+  } else
+    return (
+      <Container style={{ textAlign: 'center' }}>
+        <div style={{ marginBottom: 20 }}>
+          <br></br>
+          <h1>Welcome to </h1>{' '}
+          <h1>
+            <font style={{ color: '#35E82A', fontWeight: 'bold' }}>Pan-</font>
+            <font style={{ color: '#2EFFD5', fontWeight: 'bold' }}>Lang</font>
+          </h1>
+        </div>
+        <div>
+          <Link to={ORDER_FORM} style={{ color: 'white' }}>
+            <Button
+              style={{
+                backgroundColor: '#16AB8D',
+                borderColor: '#FFFFF5',
+                color: '#FFFFFF',
+                borderRadius: '200px',
+              }}
+              size="lg"
+              className="mb-3"
+              block
+            >
+              Start a new order
+            </Button>
+          </Link>
+        </div>
+        <Link to={STOCK} style={{ color: 'white' }}>
           <Button
             style={{
               backgroundColor: '#16AB8D',
@@ -39,11 +58,12 @@ function Home() {
             className="mb-3"
             block
           >
-            Start a new order
-          </Button>
+            Edit and add stock items
+          </Button>{' '}
         </Link>
-      </div>
-      <Link to={STOCK} style={{ color: 'white' }}>
+
+        <br></br>
+        <br></br>
         <Button
           style={{
             backgroundColor: '#16AB8D',
@@ -54,29 +74,12 @@ function Home() {
           size="lg"
           className="mb-3"
           block
+          onClick={() => auth.signOut()}
         >
-          Edit and add stock items
-        </Button>{' '}
-      </Link>
-
-      <br></br>
-      <br></br>
-      <Button
-        style={{
-          backgroundColor: '#16AB8D',
-          borderColor: '#FFFFF5',
-          color: '#FFFFFF',
-          borderRadius: '200px',
-        }}
-        size="lg"
-        className="mb-3"
-        block
-        onClick={() => auth.signOut()}
-      >
-        Sign out
-      </Button>
-    </Container>
-  );
+          Sign out
+        </Button>
+      </Container>
+    );
 }
 
 export default Home;
