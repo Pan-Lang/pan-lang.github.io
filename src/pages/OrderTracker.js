4de77b141fcb5aa70ac4ee609b5391403158416c
Renzo <renzol2@illinois.edu>
2021-01-07 19:43:32 -0600

    Style fixes

--- src/pages/OrderTracker.js
+++ src/pages/OrderTracker.js
@@ -3,60 +3,68 @@ import Container from 'react-bootstrap/Container';
 import { Button } from 'react-bootstrap';
 import { updatePerson } from '../api/People';
 import { useHistory } from 'react-router-dom';
-import { useCollection } from 'react-firebase-hooks/firestore'
-import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { db, auth } from '../firebase';
 import { LANDING } from '../constants/Routes';
 
 /**
  * Page with list of people with unfulfilled orders
  */
-
 function OrderTracker() {
-
   const history = useHistory();
-  const[user, loading_user, error_user] = useAuthState(auth);
-  const[query, setQuery] = useState();
+  const [user, userLoading, userError] = useAuthState(auth);
+  const [query, setQuery] = useState();
 
-  useEffect(() => { 
+  useEffect(() => {
     if (!Boolean(user)) {
       history.push(LANDING);
     } else {
-      setQuery(db.collection("pantries").doc(user.uid).collection("people").where("fulfilled", "==", false));
-      console.log(user.uid)
-
+      setQuery(
+        db
+          .collection('pantries')
+          .doc(user.uid)
+          .collection('people')
+          .where('fulfilled', '==', false)
+      );
+      console.log(user.uid);
     }
-  }, [history, user])
+  }, [history, user]);
 
-  const[snapshot, loading_snap, error_snap] = useCollection(query)
+  const [snapshot, snapLoading, snapError] = useCollection(query);
 
   function personFulfilled(id) {
     console.log(snapshot.size);
-    console.log("trying to fulfill", id);
+    console.log('trying to fulfill', id);
     const requestBody = {
-      pantry: user.uid, 
+      pantry: user.uid,
       _id: id,
-      fulfilled: true
+      fulfilled: true,
     };
     updatePerson(requestBody);
-  };
+  }
 
   return (
     <Container>
       <p>
-        {error_snap && <strong>Error: {JSON.stringify(error_snap)}</strong>}
-        {loading_snap && <span>Collection: Loading...</span>}
+        {userError && <strong>User Error: {JSON.stringify(snapError)}</strong>}
+        {userLoading && <span>User: Loading...</span>}
+        {snapError && <strong>Collection Error: {JSON.stringify(snapError)}</strong>}
+        {snapLoading && <span>Collection: Loading...</span>}
         {snapshot && (
           <span>
             Collection:{' '}
-            {snapshot.docs.map(doc => (
+            {snapshot.docs.map((doc) => (
               <React.Fragment key={doc.id}>
                 {JSON.stringify(doc.data())},{' '}
-                <Button onClick={() => {personFulfilled(doc.id)}}>
-                    Fulfill Person
+                <Button
+                  onClick={() => {
+                    personFulfilled(doc.id);
+                  }}
+                >
+                  Fulfill Person
                 </Button>
               </React.Fragment>
-             
             ))}
           </span>
         )}
