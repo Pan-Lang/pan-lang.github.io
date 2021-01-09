542008b221d5ebde1ef6ffb5465e2a2e0d854ff0
Kevin Zhou <qwertycowmoo@gmail.com>
2021-01-08 20:53:38 -0500

    creating pantry when signing in works

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 import firebaseConfig from './firebaseConfig.json';
+import { addPantry } from './api/Pantry';
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -9,7 +10,10 @@ firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 export const signInWithGoogle = () => {
-  auth.signInWithPopup(provider);
+  auth.signInWithPopup(provider).then(() => {
+    addPantry(auth.currentUser.uid);
+  })
+  
 };
 
 export const db = firebase.firestore();
