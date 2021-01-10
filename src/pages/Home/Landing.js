0d2a707481bc71f228ebf83cc0a5a76f3897e98c
Renzo <renzol2@illinois.edu>
2021-01-09 23:13:52 -0600

    Add slogan to landing page

--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -1,16 +1,35 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button'
+import { Link } from 'react-router-dom';
+import { SIGN_IN } from '../../constants/Routes';
+import { makeStyles } from '@material-ui/core';
 
 /**
  * Landing page for Pan-Lang.
  * The first thing people will see when visiting our web app.
  */
 function Landing() {
+  const classes = useStyles();
   return (
     <Container>
-      <p>We'll make this pretty later, just sign in.</p>
+      <Typography variant="h1">Pan-Lang</Typography>
+      <Typography variant="h2">
+        Helping the language barrier between food pantry coordinators and
+        non-English speakers.
+      </Typography>
+      <Button component={Link} variant="contained" to={SIGN_IN} className={classes.signInLink}>
+        Sign up today!
+      </Button>
     </Container>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  signInLink: {
+    marginTop: theme.spacing(3),
+  },
+}));
 
 export default Landing;
