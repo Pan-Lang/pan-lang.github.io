3600f1d132eb6c7fd6b047686940f484636cf8f4
Kevin Zhou <qwertycowmoo@gmail.com>
2021-01-21 20:17:58 -0600

    typography change

--- src/pages/Home/User.js
+++ src/pages/Home/User.js
@@ -3,9 +3,11 @@ import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core';
 import CsvModal from '../../components/CSVModal';
+import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase';
 import { ORDER_FORM, STOCK } from '../../constants/Routes';
+import theme from '../../constants/Theme';
 
 /**
  * User's home page for Pan-Lang.
@@ -17,71 +19,57 @@ function User() {
   // Handlers for showing/closing modal when downloading csv
   const handleClose = () => setShowCSVModal(false);
   const handleShow = () => setShowCSVModal(true);
-  
+
   const classes = useStyles();
   return (
-      <Container
-      align="center">
-        <div style={{ marginBottom: 20 }}>
-          <br></br>
-          <h1>Welcome to </h1>{' '}
-          <h1>
-            <font style={{ color: '#35E82A', fontWeight: 'bold' }}>Pan-</font>
-            <font style={{ color: '#2EFFD5', fontWeight: 'bold' }}>Lang</font>
-          </h1>
-        </div>
-        <div>
-          <Link to={ORDER_FORM} style={{ color: 'white' }}>
-            <Button
-              className={classes.button}
-              size="large"
-            >
-              Start a new order
-            </Button>
-          </Link>
-        </div>
-        <div>
+    <Container align="center" className={classes.root}>
+      <div style={{ marginBottom: 20 }}>
+        <Typography variant="h3" component="h1">
+          Welcome to{' '}
+          <font style={{ color: theme.palette.primary.main, fontWeight: 'bold' }}>Pan-Lang</font>
+        </Typography>
+      </div>
+      <div>
+        <Link to={ORDER_FORM} style={{ color: 'white' }}>
+          <Button className={classes.button} size="large">
+            Start a new order
+          </Button>
+        </Link>
+      </div>
+      <div>
         <Link to={STOCK} style={{ color: 'white' }}>
-          <Button
-            className={classes.button}
-            size="large"
-          >
+          <Button className={classes.button} size="large">
             Edit and add stock items
           </Button>{' '}
         </Link>
-        </div>
-        <div>
-          <Button
-            className={classes.button}
-            onClick={handleShow}
-            size="large"> Download Data
-            
-          </Button>
-        </div>
-
-        <br></br>
-        <br></br>
-        <Button
-          className={classes.button}
-          onClick={() => auth.signOut()}
-          size="large"
-        >
-          Sign out
+      </div>
+      <div>
+        <Button className={classes.button} onClick={handleShow} size="large">
+          {' '}
+          Download Data
         </Button>
-        <CsvModal
-        show={showCSVModal}
-        handleClose={handleClose}>
+      </div>
 
-        </CsvModal>
-      </Container>
-    
-    
+      <br></br>
+      <br></br>
+      <Button
+        className={classes.button}
+        onClick={() => auth.signOut()}
+        size="large"
+      >
+        Sign out
+      </Button>
+
+      {/* Download data modal */}
+      <CsvModal show={showCSVModal} handleClose={handleClose} />
+    </Container>
   );
- 
 }
 
-
 const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: 110,
+  },
   button: {
     backgroundColor: theme.palette.primary.main,
     borderColor: theme.palette.primary.borderColor,
@@ -91,9 +79,9 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor:  theme.palette.primary.dark,
     },
     borderRadius: '200px',
-    minWidth: '500px',
     margin: '5px',
+    width: '100%',
+    maxWidth: 500,
   },
-  
-}))
+}));
 export default User;
