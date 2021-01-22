cb2a31eb4321842d06ee648a6bd050add7d45291
evanmm3 <evanmm3@illinois.edu>
2021-01-21 20:11:56 -0600

    addition for landing page

--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -22,6 +22,45 @@ function Landing() {
       <Button component={Link} variant="contained" to={SIGN_IN} className={classes.signInLink}>
         Sign up today!
       </Button>
+
+      <br></br><br></br><br></br><br></br><br></br><br></br>
+      <Typography variant="h2"><b>The Problem:</b></Typography>
+      <br></br>
+      <Typography variant="h5" paragraph="true">
+        As COVID-19 wreaked havoc on daily life, food pantries were forced 
+        to implement new rules to protect the safety of their patrons. 
+        But this created much bigger problems: How will pantries quickly manage 
+        their stock in the midst of work regulations? How will non-English speaking
+        patrons find what they need without entering the pantry?
+      </Typography>
+
+      <br></br><br></br><br></br><br></br><br></br><br></br>
+      <Typography variant="h2"><b>The Solution:</b></Typography>
+      <br></br>
+      <Typography variant="h5" paragraph="true">
+        In response to this issue within our community, we created Pan-Lang,
+        a webapp designed to break language barriers and allow food pantries to
+        continue providing their services to families in need.
+      </Typography>
+      
+      <br></br><br></br>
+      <footer className={classes.footer}>
+        <Container maxWidth="lg">
+        <Typography variant="h6" align="center" gutterBottom>
+          {"Pan-Lang"}
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          {"Helping the language barrier between food pantry coordinators and non-English speakers."}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" align="center">
+          {'Copyright © '}
+          Pan-Lang{' '}
+        {new Date().getFullYear()}
+        {'.'}
+        </Typography>
+        </Container>
+      </footer>
+      <br></br>
     </Container>
   );
 }
